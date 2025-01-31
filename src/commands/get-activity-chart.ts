424255da09a1c0ff6d2b31de734bb564d395f84a
StDensity <noyaljosephnnj@gmail.com>
2025-01-31 19:16:40 +0530

    feat: Add embed support to getActivityChart command and improve response structure

--- src/commands/get-activity-chart.ts
+++ src/commands/get-activity-chart.ts
@@ -1,4 +1,4 @@
-import { Command, Option } from "discord-hono";
+import { Command, Embed, Option } from "discord-hono";
 import { factory } from "../init";
 import { InteractionContextType } from "discord-api-types/v10";
 import { formatDistanceToNow } from "date-fns";
@@ -17,8 +17,8 @@ export const getActivityChart = factory.command(
             .choices(
                { name: "last-1-hour", value: "1-hour" },
                { name: "last-6-hours", value: "6-hours" },
-               { name: "last-24-hours", value: "24-hours" },
-            //    { name: "last-7-days", value: "7-days" }
+               { name: "last-24-hours", value: "24-hours" }
+               //    { name: "last-7-days", value: "7-days" }
             )
             .required(true)
       ),
@@ -41,11 +41,11 @@ export const getActivityChart = factory.command(
             mod: 3,
          },
          // Every 2 hours in 7 days
-        //  TODO: Plan how to implement. 1. Take average of the whole day. 2. Take the highest value of the day.
-        //  "7-days": {
-        //     limit: 144,
-        //     mod: 7,
-        //  },
+         //  TODO: Plan how to implement. 1. Take average of the whole day. 2. Take the highest value of the day.
+         //  "7-days": {
+         //     limit: 144,
+         //     mod: 7,
+         //  },
       };
       const duration = (c.var.duration as keyof typeof config) || "1-hour";
 
@@ -59,8 +59,8 @@ export const getActivityChart = factory.command(
          .bind(config[duration].mod, config[duration].limit)
          .all();
 
-        //  Reverse the results to show the latest data on the right side.
-         const flippedResults = results.reverse();
+      //  Reverse the results to show the latest data on the right side.
+      const flippedResults = results.reverse();
 
       const relative_time = flippedResults.map((entry) => {
          const date = new Date(entry.timestamp as string);
@@ -73,7 +73,9 @@ export const getActivityChart = factory.command(
     labels: ${JSON.stringify(relative_time)},
     datasets: [{
       label: 'Active Players',
-      data: ${JSON.stringify(flippedResults.map((entry) => entry.active_players))}
+      data: ${JSON.stringify(
+         flippedResults.map((entry) => entry.active_players)
+      )}
     }]
   },
   
@@ -98,6 +100,24 @@ export const getActivityChart = factory.command(
          name: "chart.png",
       };
 
-      return c.res({}, fileData);
+      //   return c.res({}, fileData);
+      return c.res({
+         embeds: [
+            new Embed()
+               .title("A #MOGA Initiative")
+               .description("Powered by #MOGA Analytics")
+               .author({
+                  name: "Dr.Spy",
+                  icon_url:
+                     "https://media.discordapp.net/attachments/1241090998197686435/1241091020008198216/avatar.png?ex=664ae9d5&is=66499855&hm=8a09a8dd217dc8cddaebfa9b4bd223fa48049948fe9fedaacd0332b8e76d561d&=&format=webp&quality=lossless",
+               })
+               .image({ url: content })
+               .footer({
+                  text: '"Hey, the online count may include players in private lobbies"',
+                  icon_url:
+                     "https://media.discordapp.net/attachments/1241090998197686435/1241808909069451304/AVATAR_007_idle_source_gat_engineer.png?ex=664b8c2b&is=664a3aab&hm=3e6f28",
+               }),
+         ],
+      });
    }
 );
